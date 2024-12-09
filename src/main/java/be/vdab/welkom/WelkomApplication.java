28b3635713e3ac08a69ecff9631bd7a93f67894e
Maarten <maarten.vanpraet@vdabcampus.be>
2024-12-09 15:26:40 +0100

    Package By Feature

--- src/main/java/be/vdab/welkom/WelkomApplication.java
+++ src/main/java/be/vdab/welkom/WelkomApplication.java
@@ -3,11 +3,12 @@ package be.vdab.welkom;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class WelkomApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(WelkomApplication.class, args);
+        public static void main(String[] args) {
+            SpringApplication.run(WelkomApplication.class, args);
+        }
+
     }
 
-}
+
