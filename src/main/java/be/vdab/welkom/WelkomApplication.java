dc9bbe1ff4a4f4ff8b098d1a0d61de27a60b08f6
Maarten <maarten.vanpraet@vdabcampus.be>
2024-12-09 15:26:40 +0100

    Domain Class

--- src/main/java/be/vdab/welkom/WelkomApplication.java
+++ src/main/java/be/vdab/welkom/WelkomApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WelkomApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(WelkomApplication.class, args);
+        public static void main(String[] args) {
+            SpringApplication.run(WelkomApplication.class, args);
+        }
+
     }
 
-}
+
