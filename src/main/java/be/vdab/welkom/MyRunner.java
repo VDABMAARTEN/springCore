d2cb3695e3da4007171b09c4e30aaf02e3bbdf68
Maarten <maarten.vanpraet@vdabcampus.be>
2024-12-10 11:41:34 +0100

    Meerdere dependencies

--- src/main/java/be/vdab/welkom/MyRunner.java
+++ src/main/java/be/vdab/welkom/MyRunner.java
@@ -1,20 +1,26 @@
 package be.vdab.welkom;
 
 import be.vdab.welkom.landen.LandRepository;
+import be.vdab.welkom.talen.Taal;
+import be.vdab.welkom.talen.TaalRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyRunner implements CommandLineRunner {
     private final LandRepository landRepository;
-    public MyRunner(LandRepository landRepository) {
+    private final TaalRepository taalRepository;
+    public MyRunner(LandRepository landRepository, TaalRepository taalRepository) {
         this.landRepository = landRepository;
+        this.taalRepository = taalRepository;
     }
 
     @Override
     public void run(String... args){
         try{
             landRepository.findAll().forEach(land -> System.out.println(land.getNaam()));
+            System.out.println();
+            taalRepository.findAll().forEach(taal -> System.out.println(taal.getNaam()));
         }catch(IllegalArgumentException e){
             e.printStackTrace(System.err);
         }
